cacb47527b0f76dd7a8be80ab2ea49999bb596cf
garvbahal <garvbahal75@gmail.com>
2025-09-06 03:08:23 +0530

    create_rating_review_added

--- backend/controllers/RatingAndReview.js
+++ backend/controllers/RatingAndReview.js
@@ -1,2 +1,74 @@
 const RatingAndReview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
+
+// create rating
+exports.createRating = async (req, res) => {
+    try {
+        // get userId
+        const userId = req.user.id;
+
+        // fetch data from req.body
+        const { rating, review, courseId } = req.body;
+
+        // check if user is enrolled or not
+        const courseDetails = await Course.findOne({
+            _id: courseId,
+            studentsEnrolled: { $elemMatch: { $eq: userId } },
+        });
+        if (!courseDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Student is not enrolled in the course",
+            });
+        }
+
+        // check if user has already reviewed the course or not
+        const alreadyReviewed = await RatingAndReview.findOne({
+            user: userId,
+            course: courseId,
+        });
+
+        if (alreadyReviewed) {
+            return res.status(403).json({
+                success: false,
+                message: "Course is already reviewed by the user",
+            });
+        }
+
+        // create rating and review
+        const ratingAndReviewDetails = await RatingAndReview.create({
+            user: userId,
+            rating,
+            review,
+            course: courseId,
+        });
+
+        // add rating to course model
+        const updatedCourseDetails = await Course.findByIdAndUpdate(
+            courseId,
+            {
+                $push: {
+                    ratingAndReviews: ratingAndReviewDetails._id,
+                },
+            },
+            { new: true }
+        );
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Rating & Review has been created successfully",
+            ratingAndReviewDetails,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while creating rating&review",
+            error: err.message,
+        });
+    }
+};
+
+// get Avg rating
+
+// get All ratings
--- backend/models/RatingAndReview.js
+++ backend/models/RatingAndReview.js
@@ -15,6 +15,12 @@ const ratingAndReviewSchema = new mongoose.Schema({
         required: true,
         trim: true,
     },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+        index: true,
+    },
 });
 
 module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
