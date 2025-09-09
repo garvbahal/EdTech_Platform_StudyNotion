f0934d6ced591c343f9a224c35311ec4da0088d5
garvbahal <garvbahal75@gmail.com>
2025-09-10 00:51:34 +0530

    Update RatingAndReview.js

--- backend/controllers/RatingAndReview.js
+++ backend/controllers/RatingAndReview.js
@@ -1,5 +1,6 @@
 const RatingAndReview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
+const { default: mongoose } = require("mongoose");
 
 // create rating
 exports.createRating = async (req, res) => {
@@ -70,5 +71,79 @@ exports.createRating = async (req, res) => {
 };
 
 // get Avg rating
+exports.getAverageRating = async (req, res) => {
+    try {
+        // get course id
+        const courseId = req.body.courseId;
 
-// get All ratings
+        // calculate avg rating
+        const result = await RatingAndReview.aggregate([
+            {
+                $match: {
+                    course: new mongoose.Types.ObjectId(courseId),
+                },
+            },
+            {
+                $group: {
+                    _id: null,
+                    averageRating: {
+                        $avg: "$rating",
+                    },
+                },
+            },
+        ]);
+
+        // return rating
+        if (result.length > 0) {
+            return res.status(200).json({
+                success: true,
+                averageRating: result[0].averageRating,
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Average Rating is 0, no ratings given till now",
+            averageRating: 0,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while getting average rating",
+            error: err.message,
+        });
+    }
+};
+
+// get All ratings and Reviews
+exports.getAllRating = async (req, res) => {
+    try {
+        const allReviews = await RatingAndReview.find({})
+            .sort({
+                rating: "desc",
+            })
+            .populate({
+                path: "user",
+                select: "firstName lastName email image",
+            })
+            .populate({
+                path: "course",
+                select: "courseName",
+            })
+            .exec();
+
+        return res.status(200).json({
+            success: true,
+            message: "All reviews fetched successfully",
+            data: allReviews,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message:
+                "Something went wrong while getting all ratings and reviews",
+            error: err.message,
+        });
+    }
+};
+
+// HW: get all ratings and reviews corressponding to course id
